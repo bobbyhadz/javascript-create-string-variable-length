0793e2780439b1904ddd29863d7d545f9581f691
bobbyhadz <opmftw@gmail.com>
2024-03-01 18:08:29 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// EXAMPLE 1 - Create a String of length N filled with repeated char in JS
+
+const str = 'a'.repeat(3);
+
+console.log(str); // 👉️ 'aaa'
+
+// ------------------------------------------------------------------
+
+// // EXAMPLE 2 - Create a String of Variable Length using the Array() constructor
+
+// const arr = Array(3 + 1);
+// console.log(arr); // 👉️ [ , , , ]
+
+// const str = arr.join('a');
+// console.log(str); // 👉️ aaa
